e9e2c136c8df738ccfc666d9dd60b529bf119de0
ivo-mallee <302046344@student.rocmondriaan.nl>
2021-11-11 03:35:36 +0100

    2

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?PHP
 
+$queries = array();
+parse_str($_SERVER['QUERY_STRING'], $queries);
+@$selectedClass = $queries['Class'];
+$ShortCut = array_key_exists('Class',$queries);
 
 
 $currentWeekNumber = date('W');
+echo "Weeknummer: $currentWeekNumber <br>";
 $username = "ICT";
 $password = "!135m3n";
-$remote_url = "https://roosters.rocmondriaan.nl/ict/student/P2/$currentWeekNumber/c/c00056.htm";
-
+$i = 10;
+while (true) {
+$remote_url = "https://roosters.rocmondriaan.nl/ict/student/P2/$currentWeekNumber/c/c000$i.htm";
 // Create a stream
 $opts = array(
   'http'=>array(
@@ -16,9 +22,24 @@ $opts = array(
 );
 
 $context = stream_context_create($opts);
-echo "Weeknummer: $currentWeekNumber";
+
 // Open the file using the HTTP headers set above
 @$file = file_get_contents($remote_url, false, $context);
-if ($file) {print($file);}
-if (!$file) {echo "ERROR!";}
+
+@$Part1 = explode('<font size="6" face="Arial" color="#0000FF">',$file);
+@$Part2 = explode('&nbsp',$Part1[1]);
+@$class = $Part2[0];
+
+if (!$ShortCut) {if ($file && $class!=NULL) {echo "<a href='?Class=$class'>Create $class Shortcut</a> $file <br><br><br><br><br>";}}
+if ($ShortCut) {if ($file && $class!=NULL && str_contains($class, $selectedClass)) {echo "<a href='?Class=$class'>Create $class Shortcut</a> $file <br><br><br><br><br>";}}
+
+
+if (!$file) {break; echo "ERROR!";}
+$i++;
+}
+
+
+echo "<a href='?'>Show All Schedules</a>"
+
+
 ?>
