d173f195975457c20f20cc4e9695c3376dddf89d
ivo-mallee <302046344@student.rocmondriaan.nl>
2021-11-11 23:11:47 +0100

    Optimized Single Schedule Page

--- index.php
+++ index.php
@@ -5,15 +5,16 @@ parse_str($_SERVER['QUERY_STRING'], $queries);
 @$selectedClass = $queries['Class'];
 $ShortCut = array_key_exists('Class',$queries);
 
-
 $currentWeekNumber = date('W');
 echo "Weeknummer: $currentWeekNumber <br>";
 $username = "ICT";
 $password = "!135m3n";
 $i = 10;
 while (true) {
-$remote_url = "https://roosters.rocmondriaan.nl/ict/student/P2/$currentWeekNumber/c/c000$i.htm";
-// Create a stream
+if (!$ShortCut) {$remote_url = "https://roosters.rocmondriaan.nl/ict/student/P2/$currentWeekNumber/c/c000$i.htm";}
+if ($ShortCut) {$remote_url = "https://roosters.rocmondriaan.nl/ict/student/P2/$currentWeekNumber/c/c000$selectedClass.htm";}
+
+  // Create a stream
 $opts = array(
   'http'=>array(
     'method'=>"GET",
@@ -29,17 +30,17 @@ $context = stream_context_create($opts);
 @$Part1 = explode('<font size="6" face="Arial" color="#0000FF">',$file);
 @$Part2 = explode('&nbsp',$Part1[1]);
 @$class = $Part2[0];
-
-if (!$ShortCut) {if ($file && $class!=NULL) {echo "<a href='?Class=$class'>Create $class Shortcut</a> $file <br><br><br><br><br>";}}
-if ($ShortCut) {if ($file && $class!=NULL && str_contains($class, $selectedClass)) {echo "<a href='?Class=$class'>Create $class Shortcut</a> $file <br><br><br><br><br>";}}
-
-
+if (str_contains($file, "TD") && !$ShortCut) {echo "<a href='?Class=$i'>Create $class Shortcut</a>";}
+if (str_contains($file, "TD")) {echo "$file <br><br><br><br><br>";}
 if (!$file) {break; echo "ERROR!";}
 $i++;
+if ($ShortCut) {break;}
 }
 
 
-echo "<a href='?'>Show All Schedules</a>"
+if ($ShortCut) {echo "<a href='?'>Show All Schedules</a>";}
 
 
 ?>
+
+
